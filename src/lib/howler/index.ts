bcd3555eb32fecc2e9a70b5f9444688c60425c7b
getAxle <saihaj@getaxle.io>
2025-04-09 00:10:13 -0400

    adding background music

--- src/lib/howler/index.ts
+++ src/lib/howler/index.ts
@@ -4,7 +4,7 @@ export const FADE_DURATION = 600;
 export const VOLUME = 0.35;
 
 export const sound = new Howl({
-    src: ["/loop.webm", "/loop.mp3"],
+    src: ["/stardust.mp3"],
     loop: true,
     volume: VOLUME,
 });
