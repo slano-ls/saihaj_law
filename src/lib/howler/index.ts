2563efd335a07a068285138dc8f89f0a1440cb35
getAxle <saihaj@getaxle.io>
2025-04-09 00:15:02 -0400

    fixing up links to experience

--- src/lib/howler/index.ts
+++ src/lib/howler/index.ts
@@ -7,4 +7,10 @@ export const sound = new Howl({
     src: ["/stardust.mp3"],
     loop: true,
     volume: VOLUME,
+    onloaderror: (_, error) => {
+        console.error('Error loading sound:', error);
+    },
+    onplayerror: (_, error) => {
+        console.error('Error playing sound:', error);
+    }
 });
